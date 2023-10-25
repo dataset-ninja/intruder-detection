7bd02509a78ba4aa6d8bd8eab2ded850e0bed799
Ars Vladimirov <grokhael@gmail.com>
2023-10-25 08:11:44 +0000

    unhide

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Intruder Detection"
 PROJECT_NAME_FULL: str = "Intruder Detection"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
